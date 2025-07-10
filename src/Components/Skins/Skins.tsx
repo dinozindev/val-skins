8234e37162f8fcb62db51ec552ae0d7f1585d632
dinozin <dinozindev@gmail.com>
2025-07-10 13:55:42 -0300

    fixing search function for skins

--- src/Components/Skins/Skins.tsx
+++ src/Components/Skins/Skins.tsx
@@ -10,6 +10,14 @@ const TitleWeapon = styled.h2`
     margin: 3rem 0;
 `
 
+const FilterDiv = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 3rem;
+    gap: 1.5rem;
+`
+
 const ListWeapons = styled.ul`
     list-style: none;
     overflow: auto;
@@ -33,6 +41,17 @@ const ButtonWeapon = styled.button`
   }
 `
 
+const SkinSearchBar = styled.input`
+  color: #000;
+  width: 25%;
+  height: 2rem;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  border-radius: 1rem;
+  padding: 0.5rem;
+`
+
 const DivSkins = styled.div`
   display:flex;
   flex-wrap: wrap;
@@ -54,12 +73,6 @@ const SkinCard = styled.div`
   }
 `
 
-const SkinSearchBar = styled.input`
-  display:flex;
-  justify-content: center;
-  color: #000;
-`
-
 interface SkinsResponse {
   status: number,
   data: Skin[]
@@ -71,23 +84,23 @@ const weaponNames = ["Odin", "Ares", "Vandal", "Bulldog", "Phantom", "Judge", "B
 const Skins = () => {
 
   const [skins, setSkins] = useState<Skin[]>([]);
-  const [weapon, setWeapon] = useState<string>("Vandal");
+  const [weapon, setWeapon] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
   const [skinSearch, setSkinSearch] = useState<string>("");
 
   // removes "Random Favorite Skin" from showing.
-  const filteredSkins= skins.filter(skin => !skin.displayName.includes("Random Favorite Skin"));
+  const filteredSkins = skins.filter(skin => !skin.displayName.includes("Random Favorite Skin"));
 
   // handler for each weapon button.
   const handleWeaponSelection = (weaponName: string) => {
-    setWeapon(weaponName);
+      setWeapon(weaponName);  
   };
 
-  // filter skins by user filter (Melee is for skins that don't match any of the weapons in the weaponNames Array).
+  // filter skins by user filter (Melee is for skins that don't match any of the weapons in the weaponNames Array) and also filters by search bar.
   const filteredUserSkins = filteredSkins.filter(skin => {
     const name = skin.displayName.trim().toLowerCase();
 
-    const matchesWeapon = weapon === "Melee" 
+    const matchesWeapon = weapon === "Melee"
     ? !weaponNames.some(weaponName => 
       name.endsWith(weaponName.toLowerCase())
     )
@@ -98,6 +111,7 @@ const Skins = () => {
     return matchesWeapon && matchesSearch;
   });
 
+  // fetches all the skins from the Valorant API
   const fetchSkins = async () => {
     try {
       const { data } = await client.get<SkinsResponse>('/weapons/skins');
@@ -118,6 +132,7 @@ const Skins = () => {
   return (
     <>
       <TitleWeapon>Skins</TitleWeapon>
+      <FilterDiv>
       <ListWeapons>
         {weaponNames.map(weaponName => weapon === weaponName ? (
           <li>
@@ -134,7 +149,8 @@ const Skins = () => {
         ))
         }
       </ListWeapons>
-      <SkinSearchBar type="text" onSubmit={() => setSkinSearch}/>
+      <SkinSearchBar type="text" onChange={(e) => setSkinSearch(e.target.value)} placeholder="Search a skin by name..."/>
+      </FilterDiv>
       <DivSkins>
         {filteredUserSkins.map(skin => (
           <SkinCard key={skin.uuid}>
